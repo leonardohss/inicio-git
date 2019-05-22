947f6bb237ea95ef930ef26352f5791184964147
unknown <leorick97@gmail.com>
2019-05-21 22:58:33 -0300

    Acrescentada a mensagem de Bom dia

--- ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/Program.cs
@@ -4,6 +4,8 @@ namespace ProjetoTesteGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Bom dia");
         }
     }
 }
